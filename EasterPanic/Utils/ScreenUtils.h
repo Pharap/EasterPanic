8cf755e3357025d4dc53690feb97897cf21cdc58
Pharap <2933055+Pharap@users.noreply.github.com>
2018-03-29 00:19:51 +0100

    Add CalculateCentre function

--- EasterPanic/Utils/ScreenUtils.h
+++ EasterPanic/Utils/ScreenUtils.h
@@ -17,6 +17,11 @@ constexpr int16_t CalculateCentreY(const uint8_t & height)
 	return HalfScreenHeight - static_cast<int16_t>(round(height / 2.0));
 }
 
+constexpr int16_t CalculateCentre(const uint8_t & maxDimension, const uint8_t & dimension)
+{
+	return static_cast<int16_t>(round(maxDimension / 2.0)) - static_cast<int16_t>(round(dimension / 2.0));
+}
+
 constexpr int16_t CalculateCentreX(const uint8_t & x, const uint8_t & maxWidth, const uint8_t & width)
 {
 	return (x + ((maxWidth - x) / 2)) - static_cast<int16_t>(round(width / 2.0));
