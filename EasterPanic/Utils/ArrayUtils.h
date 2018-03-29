85587539984023f7b4f81fd5a6c95bee5fb58a65
Pharap <2933055+Pharap@users.noreply.github.com>
2018-03-29 01:08:00 +0100

    Add ArrayFirstIndex and ArrayLastIndex

--- EasterPanic/Utils/ArrayUtils.h
+++ EasterPanic/Utils/ArrayUtils.h
@@ -4,21 +4,59 @@
 // ArrayLength
 //
 
-template< typename T, size_t Size >
-constexpr size_t ArrayLength(T (&)[Size])
-{
-	return Size;
-}
-
 template< typename T >
 constexpr size_t ArrayLength(T(&)[0])
 {
 	return 0;
 }
 
+template< typename T, size_t Size >
+constexpr size_t ArrayLength(T (&)[Size])
+{
+	return Size;
+}
+
+//
+// ArrayFirstIndex
+//
+
+template< typename T >
+constexpr size_t ArrayFirstIndex(T(&)[0])
+{
+	return 0;
+}
+
+template< typename T, size_t Size >
+constexpr size_t ArrayFirstIndex(T (&)[Size])
+{
+	return 0;
+}
+
+//
+// ArrayLastIndex
+//
+
+template< typename T >
+constexpr size_t ArrayLastIndex(T(&)[0])
+{
+	return 0;
+}
+
+template< typename T, size_t Size >
+constexpr size_t ArrayLastIndex(T (&)[Size])
+{
+	return Size - 1;
+}
+
 //
 // StringLength
 //
+
+template< typename T >
+constexpr size_t StringLength(T (&)[0])
+{
+	return 0;
+}
 
 template< typename T, size_t Size >
 constexpr size_t StringLength(T (&)[Size])
