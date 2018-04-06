8bf73f6f3d3e40cd0be725ad667bffb09725e564
Pharap <2933055+Pharap@users.noreply.github.com>
2018-04-06 05:57:55 +0100

    Add more string measurement functions

--- EasterPanic/Utils/FontUtils.h
+++ EasterPanic/Utils/FontUtils.h
@@ -9,24 +9,47 @@ constexpr const static uint8_t FontSpaceWidth = 1;
 constexpr const static uint8_t FontSpaceHeight = 1;
 constexpr const static uint8_t FontLineHeight = FontCharHeight + FontSpaceHeight;
 
-template< typename T, size_t N > constexpr size_t StringWidth(T (&string)[N])
+constexpr inline uint8_t StringWidth(const uint8_t & charCount)
 {
-	return (StringLength(string) * FontCharWidth) + ((StringLength(string) - 1) * FontSpaceWidth);
+	return (charCount * FontCharWidth) + ((charCount - 1) * FontSpaceWidth);
 }
 
-constexpr uint8_t StringHeight(uint8_t lineCount)
+constexpr inline size_t StringWidth(const size_t & charCount)
+{
+	return (charCount * FontCharWidth) + ((charCount - 1) * FontSpaceWidth);
+}
+
+constexpr inline size_t StringWidth(const int & charCount)
+{
+	return StringWidth(static_cast<size_t>(charCount));
+}
+
+constexpr inline uint8_t StringHeight(const uint8_t & lineCount)
 {
 	return (lineCount * FontCharHeight) + ((lineCount - 1) * FontSpaceHeight);
 }
 
-inline size_t RuntimeStringWidth(const char * string)
+constexpr inline size_t StringHeight(const size_t & lineCount)
 {
-	auto length = strlen(string);
-	return (length * (FontCharWidth)) + ((length - 1) * FontSpaceWidth);
+	return (lineCount * FontCharHeight) + ((lineCount - 1) * FontSpaceHeight);
+}
+
+constexpr inline size_t StringHeight(const int & lineCount)
+{
+	return StringHeight(static_cast<size_t>(lineCount));
+}
+
+template< typename T, size_t N > constexpr size_t StringWidth(T (&string)[N])
+{
+	return StringWidth(StringLength(string));
 }
 
 inline size_t ProgmemStringWidth(const char * string)
 {
-	auto length = strlen_P(string);
-	return (length * (FontCharWidth)) + ((length - 1) * FontSpaceWidth);
+	return StringWidth(strlen_P(string));
+}
+
+inline size_t RuntimeStringWidth(const char * string)
+{
+	return StringWidth(strlen(string));
 }
